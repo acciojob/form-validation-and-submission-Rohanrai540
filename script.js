f9c16cf15a157ec62664a9323c8230bc9f375f43
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-03-22 21:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 //your JS code here. If required.
-<script>
-  const checkbox = document.getElementById("termsCheckbox");
-  const submitBtn = document.getElementById("submitBtn");
-
-  checkbox.addEventListener("change", function () {
-    submitBtn.disabled = !this.checked;
-  });
-</script>
